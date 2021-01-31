f87b22efc9f9c7c8d49a928878878a596fb89ba9
monicaliz <monicaeliz3@gmail.com>
2021-01-31 17:04:46 -0500

    commit tree 1b4236fa0f2a46dab2d537cc0390566bcf52ee53

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+#define 'Dog' class
+class Dog
+    #define instance method 'bark'
+def bark
+    puts "Woof!"
+end
+
+#define instance method 'sit'
+def sit
+    puts "The Dog is sitting"
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+#define "Person" class
+class Person
+    #define instance method "talk"
+    def talk
+        puts "Hello World!"
+    end
+
+    #define instance method "walk"
+    def walk
+        puts "The Person is walking"
+    end
+
+end
